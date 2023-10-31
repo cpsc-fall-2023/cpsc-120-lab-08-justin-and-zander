ba62c8d8d39b9d45f94716a07bb9e2358ad18509
Heinlein <justinheinlein4@cs300u-4c.acad.fullerton.edu>
2023-10-30 19:26:07 -0700

    I'm find btw

--- part-2/average.cc
+++ part-2/average.cc
@@ -1,4 +1,7 @@
-// TODO: Add the required header
+// Justin Heinlein
+// justinheinlein4@csu.fullerton.edu
+// @justinheinlein4
+// Partners: @imor128, @Zandercbrooks
 
 #include <iostream>
 #include <string>
@@ -6,35 +9,23 @@
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> arguments{argv, argv + argc};
-
-  // TODO: Validate that there is at least one command line argument.
-  // If not, print an error message and return a non-zero value.
-  if(arguments.size() < 0)
-  {
-    std::cout << "Error provide at least one argument";
-    return -1;
+  int args = static_cast<int>(arguments.size());
+  if (args == 1) {
+    std::cout << "error: you must supply at least one number\n";
+    return 1;
   }
-  // TODO: Write a for-each loop to sum (add up) all of the command line
-  // arguments.
-  // Use a double or float type so that your program preserves fractional
-  // values.
-  // The loop needs to skip over the command name, which is the first element
-  // of the arguments vector.
-  // Each argument is a std::string. You will need to convert each string into
-  // a number with the std::stod or std::stof function.
+
   double sum_of_arguments{0};
-  for(int i = 1; i < arguments.size(); i++)
-  {
-    sum_of_arguments += stod(arguemts.at(i));
-  } 
-  // TODO: After the loop has finished summing the arguments, calculate the
-  // average of the values. Recall that the average is the total value divided
-  // by the number of values.
+  bool first = true;
+  for (const std::string& arg : arguments) {
+    if (first) {
+      first = false;
+      continue;
+    }
+    sum_of_arguments += stod(arg);
+  }
   double average{0};
-  average = sum_of_arguments / (arguments.size() - 1);
-  // TODO: Use cout to print out a message of the form
-  // average = *AVERAGE*
-  // on its own line.
+  average = sum_of_arguments / (args - 1);
   std::cout << "average = " << average << "\n";
   return 0;
 }
