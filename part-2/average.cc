6ab96d7a807fbadc1adffc7e702db459484f20df
Zander Brooks <Zandercbrooks@csu.fullerton.edu>
2023-10-30 18:30:55 -0700

    l

--- part-2/average.cc
+++ part-2/average.cc
@@ -22,14 +22,19 @@ int main(int argc, char* argv[]) {
   // of the arguments vector.
   // Each argument is a std::string. You will need to convert each string into
   // a number with the std::stod or std::stof function.
-
+  double sum_of_arguments{0};
+  for(int i = 1; i < arguments.size(); i++)
+  {
+    sum_of_arguments += stod(arguemts.at(i));
+  } 
   // TODO: After the loop has finished summing the arguments, calculate the
   // average of the values. Recall that the average is the total value divided
   // by the number of values.
-
+  double average{0};
+  average = sum_of_arguments / (arguments.size() - 1);
   // TODO: Use cout to print out a message of the form
   // average = *AVERAGE*
   // on its own line.
-
+  std::cout << "average = " << average << "\n";
   return 0;
 }
